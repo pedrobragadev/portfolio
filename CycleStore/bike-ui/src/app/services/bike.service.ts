d3a689db78c8ab18ebdbb1a27eabcd783c640a1f
pedro.braga <pedrobragaom@gmail.com>
2019-10-18 19:52:52 -0300

    Created home component in Angular

--- CycleStore/bike-ui/src/app/services/bike.service.ts
+++ CycleStore/bike-ui/src/app/services/bike.service.ts
@@ -16,4 +16,13 @@ export class BikeService {
   getBikes() {
     return this.http.get('/server/api/v1/bikes');
   }
+
+  getBike(id: number) {
+    return this.http.get('/server/api/v1/bikes/' + id);
+  }
+
+  insertBike(bike) {
+    const body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes/', body, httpOptions);
+  }
 }
