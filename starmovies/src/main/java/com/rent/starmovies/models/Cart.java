3ea1b4655fe6ccb0f2ac971b365759d088ebb593
pedro.braga <pedrobragaom@gmail.com>
2019-10-24 17:28:16 -0300

    Initial commit - created models

---
+++ starmovies/src/main/java/com/rent/starmovies/models/Cart.java
@@ -0,0 +1,62 @@
+package com.rent.starmovies.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Cart {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private Client client;
+    private List<Item> itens;
+    private Date date;
+    private Double totalValue;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Item> itens) {
+        this.itens = itens;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+}
