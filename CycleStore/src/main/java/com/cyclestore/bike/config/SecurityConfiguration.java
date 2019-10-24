21851651eab8db89885b2b9ba26c7a7ad601551a
pedro.braga <pedrobragaom@gmail.com>
2019-10-23 21:09:27 -0300

    V2 - remove AuthO and another stuffs

--- CycleStore/src/main/java/com/cyclestore/bike/config/SecurityConfiguration.java
+++ CycleStore/src/main/java/com/cyclestore/bike/config/SecurityConfiguration.java
@@ -11,19 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Value(value = "${auth0.apiAudience}")
-    private String apiAudience;
-    @Value(value = "${auth0.issuer}")
-    private String issuer;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        JwtWebSecurityConfigurer.forRS256(apiAudience, issuer)
-                .configure(http)
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/v1/bikes").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/bikes").hasAuthority("view.registrations")
-                .antMatchers(HttpMethod.GET, "/api/v1/bikes/**").hasAuthority("view.registration")
-                .anyRequest().authenticated();
-    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        JwtWebSecurityConfigurer.forRS256(apiAudience, issuer)
+//                .configure(http)
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/api/v1/bikes").permitAll()
+//                .antMatchers(HttpMethod.GET, "/api/v1/bikes").hasAuthority("view.registrations")
+//                .antMatchers(HttpMethod.GET, "/api/v1/bikes/**").hasAuthority("view.registration")
+//                .anyRequest().authenticated();
+//    }
 }
