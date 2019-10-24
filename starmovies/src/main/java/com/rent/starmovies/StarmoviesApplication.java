302deb02ac81eda8b83d765b4a467575dd7ddc72
pedro.braga <pedrobragaom@gmail.com>
2019-10-24 12:27:52 -0300

    Initial commit

---
+++ starmovies/src/main/java/com/rent/starmovies/StarmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.rent.starmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarmoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarmoviesApplication.class, args);
+    }
+
+}
---
+++ starmovies/src/test/java/com/rent/starmovies/StarmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.starmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarmoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
