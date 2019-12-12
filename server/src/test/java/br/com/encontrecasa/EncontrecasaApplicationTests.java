27411a5cc20c67973166bd37dcafbdf9eb556fb8
pedro.braga <pedrobragaom@gmail.com>
2019-12-12 08:28:38 -0300

    First commit for this project

---
+++ server/src/test/java/br/com/encontrecasa/EncontrecasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.encontrecasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncontrecasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
