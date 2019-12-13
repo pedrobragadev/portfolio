71c15f2b02f9815b45e3e9e855a896b7b9706821
pedro.braga <pedrobragaom@gmail.com>
2019-12-13 14:40:29 -0300

    Reading spring documentation

--- server/src/main/java/br/com/encontrecasa/EncontrecasaApplication.java
+++ server/src/main/java/br/com/encontrecasa/EncontrecasaApplication.java
@@ -1,10 +1,19 @@
 package br.com.encontrecasa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EncontrecasaApplication {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EncontrecasaApplication.class, args);
